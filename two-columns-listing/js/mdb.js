5976c0d001568cc527fd800af91c8251afcf2c6e
Bartek Malanowski <b.malanowski@mdbootstrap.com>
2018-05-07 12:48:37 +0200

    4.5.2 release

--- two-columns-listing/js/mdb.js
+++ two-columns-listing/js/mdb.js
@@ -1,6 +1,6 @@
 /*!
  * Material Design for Bootstrap 4
- * Version: MDB FREE: 4.5.1
+ * Version: MDB FREE: 4.5.2
  *
  *
  * Copyright: Material Design for Bootstrap
@@ -26,7 +26,7 @@
  */
 
 
-/*
+/*d 
 
   jquery.easing.js
   velocity.min.js
